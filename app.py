de55c4e14ba6d6e7ea868a4a1b169ecbcbf6b1e1
Gerardo Moreno <gmoreno9617@gmail.com>
2020-03-11 23:16:37 +0000

    Added external API and DELETE

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask, render_template, request, jsonify
 import json
 import requests
 import requests_cache
+import pprint
+
 requests_cache.install_cache('nutrition_app_cache', backend='sqlite', expire_after=36000)
 
-all_records = [
-    {
-    
-    }
-]
+all_records = [{"age":"23","height":"1.70","name":"Name","sex":"male","surname":"Lastname","weight":"58"}]
 
 app = Flask(__name__)
 
@@ -18,8 +16,8 @@ def hello():
 
 @app.route('/new_client', methods=['POST'])
 def create_new_client():
-    if not request.json or not 'name' in request.json or not "surname" in request.json or not "age" in request.json or not "sex" in request.json or not "weight" in request.json or not "height" in request.json:
-        return jsonify({'error':'the new record needs to have name\nsurname\nage\nsex\nweight\nheight'}), 400
+    if not request.json or not "name" in request.json or not "surname" in request.json or not "age" in request.json or not "sex" in request.json or not "weight" in request.json or not "height" in request.json:
+        return jsonify({'error':'the new record needs to have name,surname,age,sex,weight,height'}), 400
     new_record = {
         'name': request.json['name'],
         'surname': request.json['surname'],
@@ -34,6 +32,24 @@ def create_new_client():
 @app.route('/all', methods=['GET'])
 def get_everything():
     return jsonify(all_records)
+
+@app.route('/external_routines', methods=['GET'])
+def get_external_workout():
+    workouts_template = 'https://wger.de/api/v2/exercise/?language=2'
+    resp = requests.get(workouts_template)
+    if resp.ok:
+        workouts = resp.json()
+    else:
+        print(resp.reason)
+    return workouts
+
+@app.route('/delete_user/<client_name>/<client_surname>',methods=['DELETE'])
+def delete_user(client_name,client_surname):
+    matching_user = [user for user in all_records if user["name"] == client_name and user["surname"] == client_surname]
+    if len(matching_user)==0:
+        return jsonify({ ' error ' : 'user not found! ' }), 404
+    all_records.remove(matching_user[0])
+    return jsonify({ ' success ' : True})
 
 if __name__=="__main__":
     app.run(host='127.0.0.1')
