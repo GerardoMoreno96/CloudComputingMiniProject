4670dd36d9540e7e6e581b107e220e1db804e577
Gerardo Moreno <gmoreno9617@gmail.com>
2020-03-26 21:59:03 +0000

    Cassandra Support

--- app.py
+++ app.py
@@ -1,36 +1,52 @@
 from flask import Flask, render_template, request, jsonify
+from cassandra.cluster import Cluster
 import json
 import requests
 import requests_cache
-
-requests_cache.install_cache('nutrition_app_cache', backend='sqlite', expire_after=36000)
-
-all_records = [{"age":"23","height":"1.70","name":"Name","sex":"male","surname":"Lastname","weight":"58"}]
+requests_cache.install_cache('gymprogress_app_cache', backend='sqlite', expire_after=36000)
+cluster = Cluster(contact_points=['172.17.0.2'],port=9042)
+session = cluster.connect()
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return('<h1>Welcome to Gerardo\'s Nutritional App</h1>')
+    return('<h1>Welcome to Gerardo\'s Gym Progress App</h1>')
 
 @app.route('/new_client', methods=['POST'])
 def create_new_client():
-    if not request.json or not "name" in request.json or not "surname" in request.json or not "age" in request.json or not "sex" in request.json or not "weight" in request.json or not "height" in request.json:
+    if not request.json or not "name" in request.json or not "surname" in request.json \
+        or not "age" in request.json or not "sex" in request.json or not "weight" in request.json or not "height" in request.json:
         return jsonify({'error':'the new record needs to have name,surname,age,sex,weight,height'}), 400
     new_record = {
         'name': request.json['name'],
         'surname': request.json['surname'],
-        'age': request.json['age'],
+        'age': int(request.json['age']),
         'sex': request.json['sex'],
-        'weight':request.json['weight'],
-        'height': request.json['height']
+        'weight': float(request.json['weight']),
+        'height': float(request.json['height'])
     }
-    all_records.append(new_record)
-    return jsonify({'message': 'created: /new_client'.format(new_record['name'])}), 201
+    session.execute( """INSERT INTO gym.users(name,surname,age,sex,weight,height) VALUES ( '{}','{}',{},'{}',{},{})""".format\
+        (new_record['name'],new_record['surname'],new_record['age'],new_record['sex'],new_record['weight'],new_record['height'] ))
+    
+    return jsonify({'message': 'created: /new_client {}'.format(new_record['name'])}), 201
 
 @app.route('/all', methods=['GET'])
 def get_everything():
-    return jsonify(all_records)
+    rows = session.execute( """ SELECT * FROM gym.users""")
+    result=[]
+    for r in rows:
+        result.append(
+            {
+                'name': r.name,
+                'surname': r.surname,
+                'age': r.age,
+                'sex': r.sex,
+                'weight': r.weight,
+                'height': r.height
+            }
+        )
+    return jsonify(result)
 
 @app.route('/external_routines', methods=['GET'])
 def get_external_workout():
@@ -42,13 +58,17 @@ def get_external_workout():
         print(resp.reason)
     return workouts
 
-@app.route('/delete_user/<client_name>/<client_surname>',methods=['DELETE'])
-def delete_user(client_name,client_surname):
-    matching_user = [user for user in all_records if user["name"] == client_name and user["surname"] == client_surname]
-    if len(matching_user)==0:
-        return jsonify({ ' error ' : 'user not found! ' }), 404
-    all_records.remove(matching_user[0])
-    return jsonify({ ' success ' : True})
+@app.route('/delete_user',methods=['DELETE'])
+def delete_user():
+    session.execute( """DELETE FROM gym.users WHERE name={} AND surname={})""".format\
+        (request.json['name'],request.json['surname']))
+    return jsonify({'message': 'deleted: /user/{},{}'.format\
+        (request.json['name'],request.json['surname'])}),200
+    # matching_user = [user for user in all_records if user["name"] == client_name and user["surname"] == client_surname]
+    # if len(matching_user)==0:
+    #     return jsonify({ ' error ' : 'user not found! ' }), 404
+    # all_records.remove(matching_user[0])
+    
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=5000)
